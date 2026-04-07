1f5dd715c3627db87e3ddb702980c56104064be1
jakkalilokesh <jakkalilokesh@gmail.com>
2026-04-07 11:32:15 +0530

    Updated TheFile Python

--- app.py
+++ app.py
@@ -13,3 +13,16 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n - 1)
+    
+
+
+number = 5
+result = factorial(number)
+print(f"The factorial of {number} is: {result}")    
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
